2149480cf9f6a6caf7798fa3e6588a8cefef3c53
Om <oma2668@gmail.com>
2020-05-03 16:05:09 -0700

    Add files via upload
    Fixed metadata issue.
    Added Documentation and Examples.

--- os_server_snapshot.py
+++ os_server_snapshot.py
@@ -14,18 +14,62 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
                     'supported_by': 'community'}
 
+DOCUMENTATION='''
+---
+module: os_serve_snapshot
+short_description: Create snapshots of Instances in OpenStack
+extends_documentation_fragment: openstack
+description:
+    - Create snapshots of an instance in OpenStack.
+options:
+    name:
+      description:
+         - Name that has to be given to the Snapshot.
+      required: True
+    server:
+      description:
+         - The name or id of the instance to be snapshotted.
+      required: True
+    wait:
+      description:
+         - If the module should wait for the snapshot to be created.
+      type: bool
+      default: 'yes'
+    timeout:
+      description:
+         - The amount of time the module should wait for the instance to get
+          into active state.
+      default: 180
+    meta:
+      description:
+         - 'A list of key value pairs that should be provided as a metadata to
+           the new instance or a string containing a list of key-value pairs.
+           Eg:  meta: "key1=value1,key2=value2"'
+'''
+EXAMPLES = '''
+- name: Create a snapshot of an instance.
+  os_server_snapshot:
+       auth:
+         auth_url: https://identity.example.com
+         username: admin
+         password: admin
+         project_name: admin
+       name: Snapshot-1
+       server: vm1
+       timeout: 200
+       meta:
+         group: vm1-snaps
+'''
+
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.openstack import (
     openstack_cloud_from_module, openstack_module_kwargs,
     openstack_full_argument_spec )
     
-def _exit_hostvars(module, cloud, server, changed=True):
-    hostvars = cloud.get_openstack_vars(server)
-    module.exit_json(
-        changed=changed, server=server, id=server.id, openstack=hostvars)
-        
 def _parse_meta(meta):
     if isinstance(meta, str):
+        print('True')
+        print('Entered Loop')
         metas = {}
         for kv_str in meta.split(","):
             k, v = kv_str.split("=")
@@ -43,9 +87,8 @@ def _create_server_snapshot(module, cloud):
         if not server_dict:
             module.fail_json(msg="Could not find server %s" % server)
 
-    module.params['meta'] = _parse_meta(module.params['meta'])
+    meta  = _parse_meta(module.params['meta'])
     
-    meta = module.params['meta']
     server_snapshot = cloud.create_image_snapshot(
     	name=module.params['name'],
     	server=module.params['server'],
